deef8056902298b71f317686e7f930370598e462
destini <40705170+destinideinde@users.noreply.github.com>
2020-08-22 01:35:03 -0400

    Add ground plane with a texture.
    Change image later as needed
    Completes requirement (2/7)

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ import "@babylonjs/core/Materials/standardMaterial";
 
 // Required side effects to populate the Create methods on the mesh class. Without this, the bundle would be smaller but the createXXX methods from mesh would not be accessible.
 import {MeshBuilder} from  "@babylonjs/core/Meshes/meshBuilder";
+import { StandardMaterial } from "@babylonjs/core/Materials/standardMaterial";
+import { Texture } from "@babylonjs/core/Materials/Textures/texture";
 
 const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement; // Get the canvas element 
 const engine = new Engine(canvas, true); // Generate the BABYLON 3D engine
@@ -25,6 +27,21 @@ class Playground {
         var camera = new UniversalCamera("UniversalCamera", new Vector3(0, 0, -10), scene);
         camera.setTarget(Vector3.Zero());
         camera.attachControl(canvas, true);
+
+        // (2) Create a ground plane w/ interesting material.
+        var groundPlane = MeshBuilder.CreatePlane(
+                "ground plane", 
+                { 
+                    width : 5,
+                    height : 5
+                },
+                scene);
+        var groundMaterial = new StandardMaterial("textures/groundTextureSrc", scene);
+        groundMaterial.diffuseTexture = new Texture("textures/groundTextureSrc.png", scene);
+        groundMaterial.specularTexture = new Texture("textures/groundTextureSrc.png", scene);
+        groundMaterial.emissiveTexture = new Texture("textures/groundTextureSrc.png", scene);
+        groundMaterial.ambientTexture = new Texture("textures/groundTextureSrc.png", scene);
+        groundPlane.material = groundMaterial;
 
         // Add lights to the scene
         new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
